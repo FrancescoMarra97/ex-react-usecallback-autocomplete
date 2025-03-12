30df1fbbc0edb543971f748db0364c83a3ddba5d
FrancescoMarra97 <francescomarra192@gmail.com>
2025-03-12 16:34:14 +0100

    any fixes

--- ex-react-usecallback-autocomplete/src/App.jsx
+++ ex-react-usecallback-autocomplete/src/App.jsx
@@ -1,34 +1,53 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import './App.css'
+
+function debounce(callback, delay) {
+  let timer;
+  return (value) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      callback(value)
+    }, delay)
+  }
+}
 
 function App() {
   const [query, setQuery] = useState("")
   const [results, setResults] = useState([])
+  console.log(results);
 
-  useEffect(() => {
-    if (query === "") {
+  const fetchResults = async (query) => {
+    if (query.trim() === "") {
       setResults([])
       return
     }
+    try {
+      const res = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`)
+      const data = await res.json()
+      setResults(data)
+    } catch (error) {
+      console.error(error)
+    }
 
-    fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`)
-      .then(res => res.json())
-      .then(data => setResults(data))
-      .catch(err => console.error(err))
+
+  }
+  useEffect(() => {
+    fetchResults(query)
   }, [query])
   return (
     <>
       <div className="container">
-        <input type="text"
+        <h1>Autocomplete</h1>
+        <input className='d-flex justify-content-center m-3' type="text"
           placeholder='Cerca...'
           value={query}
           onChange={(e) => setQuery(e.target.value)} />
 
         {query && results.length > 0 && (
           <ul className='results-dropdown'>
-            {results.map((result, i) => (
-              <li key={i}>{result.name}</li>
+            {results.map((result) => (
+              <li key={result.id}>{result.name}</li>
             ))}
           </ul>
         )}
